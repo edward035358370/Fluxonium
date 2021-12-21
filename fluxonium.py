17b7e566d8f831255df1b401c02385abb2449afb
edward035358370 <43946036+edward035358370@users.noreply.github.com>
2021-12-21 16:33:56 +0800

    Add files via upload
    add the function for solving the pure dephasing time, relaxation time from quasi particle and dielectric

--- fluxonium.py
+++ fluxonium.py
@@ -10,7 +10,9 @@ from scipy.linalg import expm
 from inspect import getfullargspec as showarg
 
 class Fluxonium():
-    def __init__(self,Ej,Ec,El,cutoff):
+    def __init__(self,Ej,Ec,El,cutoff,start = -1,
+                                      stop = 1,
+                                      points = 201):
         """
 
         Parameters
@@ -28,12 +30,13 @@ class Fluxonium():
         self.Ej = Ej
         self.Ec = Ec
         self.El = El
-
         self.cutoff = cutoff
-    
-    def get_x(self,start = -1,
-                   stop = 1,
-                   points = 201):
+        self.start = start
+        self.stop = stop
+        self.points = points
+        self.x = np.linspace(self.start,self.stop,self.points)
+        
+    def get_x(self):
         """
         Formatted external flux phi
     
@@ -50,32 +53,32 @@ class Fluxonium():
         np.array
     
         """
-        self.x = np.linspace(start,stop,points)
+        self.x = np.linspace(self.start,self.stop,self.points)
         return self.x
     
     def hamiltonian_exp(self,phi):
-            """
-            Return the Hamiltonian matrix with energy in GHz calculated with
-            the exponential method.
-            
-            H = 4Ec|n_op><n_op| + 0.5El|phi_op><phi_op| - Ej*cos(phi_op+phi)
-            
-            Parameters
-            ----------
-            phi : float
-                Reduced Φ_ext/Φ_0.
-            Returns
-            ----------
-            Hamiltonian : np.matrix
-                Hamiltonian matrix.
-            """
-            n_square = np.dot(self.op_n(),self.op_n())
-            phi_square = np.dot(self.op_phi(),self.op_phi())
-            cos_ex =  1./2.*np.exp(-1j*phi*np.pi*2.)*expm(1j*self.op_phi())\
-                     +1./2.*np.exp(1j*phi*np.pi*2.)*expm(-1j*self.op_phi())
-            
+        """
+        Return the Hamiltonian matrix with energy in GHz calculated with
+        the exponential method.
+        
+        H = 4Ec|n_op><n_op| + 0.5El|phi_op><phi_op| - Ej*cos(phi_op+phi)
+        
+        Parameters
+        ----------
+        phi : float
+            Reduced Φ_ext/Φ_0.
+        Returns
+        ----------
+        Hamiltonian : np.matrix
+            Hamiltonian matrix.
+        """
+        n_square = np.dot(self.op_n(),self.op_n())
+        phi_square = np.dot(self.op_phi(),self.op_phi())
+        cos_ex =  1./2.*np.exp(-1j*phi*np.pi*2.)*expm(1j*self.op_phi())\
+                 +1./2.*np.exp(1j*phi*np.pi*2.)*expm(-1j*self.op_phi())
+        
 
-            return 4*self.Ec*n_square + 0.5*self.El*phi_square -self.Ej*cos_ex
+        return 4*self.Ec*n_square + 0.5*self.El*phi_square -self.Ej*cos_ex
         
     def op_phi(self):
         """
@@ -121,6 +124,11 @@ class Fluxonium():
         phi_0 = (self.El/(8*self.Ec))**(1./4.)
         return 1j*phi_0/np.sqrt(2.)*(c_p - c_m)
     
+    def op_sin_phi(self,phi_ext):
+        ope = 1.0j * (self.op_phi() + np.eye(self.cutoff+1)*phi_ext*2*np.pi)
+        sine_ope = (expm(ope/2.0) - expm(-ope/2.0))/2j
+        
+        return sine_ope
     def eigen(self,H):
         """
         extracte the eigen value and eigen state
@@ -135,7 +143,7 @@ class Fluxonium():
         v : eigen vector  2D array
 
         """
-        w, v = np.linalg.eigh(H)
+        w, v = np.linalg.eig(H)
         
         idx = w.argsort()
         w = np.sort(w)
@@ -216,19 +224,26 @@ class Fluxonium():
         np.array 2D array
 
         """
+        k = 0
         if operator == "phi":
             operate = self.op_phi()
         elif operator == "n":
             operate = self.op_n()
+        elif operator == "sin(phi/2)":
+            k = 1
             
         
-        matrix_element = np.zeros((len(bra_ket),len(self.x)))
+        matrix_element = np.zeros((len(bra_ket),self.points))
         
         for colum, i in  enumerate(self.x):
+            
             H = self.hamiltonian_exp(i)
             _,temp = self.eigen(H)
+            if k == 1:
+                operate = self.op_sin_phi(i)
             for roll, transition in enumerate(bra_ket):
                 matrix_element[roll][colum] = abs(np.dot(temp[:][transition[0]].T,np.dot(operate,temp[:][transition[1]])))
+                
         return np.array(matrix_element)
     
     def T1_die_loss(self,El_array):
@@ -252,7 +267,7 @@ class Fluxonium():
         T = 0.01 #10 mk
         h_bar = 6.62e-34/(2*np.pi)    #Placnk's reduce constant
         
-        res = np.zeros((len(El_array),len(self.x)))
+        res = np.zeros((len(El_array),self.points))
         for index,i in enumerate(El_array):
             self.El = i
             w = self.eigen_spectrum(3,[True,True])[1]*1e9*2*np.pi
@@ -269,6 +284,66 @@ class Fluxonium():
             res[index] = np.log10(1e6/(2*np.pi*gamma_cap))
         return res
     
+    def T_qp_sing_junc(self,xqp_list,transition = [0,1]):
+        
+        relax_time_qp = np.zeros((len(xqp_list),self.points))
+        h = 6.626e-34
+        e = 1.6e-19
+        gap = 93e-6*e
+        for idx,xqp in enumerate(xqp_list):
+            temp = (self.matrix_ele('sin(phi/2)',[transition])[0])**2
+            E01 = self.eigen_spectrum(9,transition = [True,True])[transition[1]]*1e9
+            constant = 8*self.Ej*h*1e9*xqp/(h*0.5)
+            constant = constant*(2*gap/(h*E01))**0.5
+            relaxa_qp = temp*constant
+            relax_time_qp[idx] = 1e6/relaxa_qp
+        return np.log10(relax_time_qp)
+    def T_qp_array(self,xqp_list,transition = [0,1]):
+        
+        relax_time_qp = np.zeros((len(xqp_list),self.points))
+        h = 6.626e-34
+        e = 1.6e-19
+        gap = 93e-6*e
+        for idx,xqp in enumerate(xqp_list):
+            temp = (self.matrix_ele('phi',[transition])[0]/2)**2
+            E01 = self.eigen_spectrum(9,transition = [True,True])[transition[1]]*1e9
+            constant = 8*self.Ej*h*1e9*xqp/(h*0.5)
+            constant = constant*(2*gap/(h*E01))**0.5
+            relaxa_qp = temp*constant
+            relax_time_qp[idx] = 1e6/relaxa_qp
+        return np.log10(relax_time_qp)
+    def T_dephasing(self,transition = [1]):
+        """
+        
+
+        Parameters
+        ----------
+        transition : list
+            decide the transition you want.
+            ex.
+                0--->1: 1
+                0--->2: 2.....
+            transition = [1,2]
+        Returns
+        -------
+        None.
+
+        """
+        dephase_time = np.zeros((len(transition),self.points-1)) # points -1 is for differential
+        flux_noise_amp = (4.3e-6)**2 #set by the transmon experiment
+        energy = self.eigen_spectrum(9,[True,True])
+        
+        for i in transition:
+            E = energy[i]
+            phi_ext = self.x
+            E_diff = np.diff(E)/np.diff(phi_ext)
+            E_diff_const = 2*np.pi*1e9*(flux_noise_amp*np.log(2))**0.5
+            dephase_rate = np.abs(E_diff)*E_diff_const
+            
+            dephase_time[i-1] = 1/dephase_rate
+        self.x = np.linspace(self.start,self.stop,self.points-1) #remove one point for plot
+        return dephase_time
+    
     def function_mappings(self):
         """
         str to func mappings, the IO for Plot1DSpectrum
@@ -276,12 +351,14 @@ class Fluxonium():
         return {
             'eigen_spectrum': self.eigen_spectrum,
             'matrix_ele': self.matrix_ele,
-            'T1_die_loss': self.T1_die_loss
+            'T1_die_loss': self.T1_die_loss,
+            'T_dephasing': self.T_dephasing,
+            'T_qp_sing_junc': self.T_qp_sing_junc,
+            'T_qp_array': self.T_qp_array
             }
-
+            
     def setFunc(self,
-            func: str, funcArg: list,
-            start: float = -1, stop: float = 1,points: int = 201
+            func: str, funcArg: list
             ):
         """
         Shape function wrapper.
@@ -309,7 +386,7 @@ class Fluxonium():
     
         generator = {
             'function': func.__name__, #__name__is function's name
-            'X': {'start': start, 'stop': stop, 'points': points},
+            'X': {'start': self.start, 'stop': self.stop, 'points': self.points},
             'Y': dict(zip(argNames, funcArg))
             }
         return generator
@@ -331,13 +408,7 @@ class Fluxonium():
             y (amplitude) data.
     
         """
-        
-        # x
-        start = generator['X']['start']
-        stop = generator['X']['stop']
-        points = generator['X']['points']
-        self.get_x(start,stop,points)
-        
+
         # y
         func = generator['function']
         if isinstance(func, str):
@@ -349,7 +420,7 @@ class Fluxonium():
         
         return self.x, y
     
-    def plot1Dspectrum(self,func_name, argdict,start = -1,stop = 1,points = 101):
+    def plot1Dspectrum(self,func_name, argdict):
         """
         plot the data can be count, and the way of parameters are counted
         is given by parse and setFunc
@@ -376,7 +447,7 @@ class Fluxonium():
         """
         from pylab import figure,plot, show
         
-        generator = self.setFunc(func_name, argdict,start,stop,points)
+        generator = self.setFunc(func_name, argdict)
         x,y = self.parse(generator)
         
         figure()
@@ -387,26 +458,43 @@ class Fluxonium():
     
 if __name__ == "__main__":
     """
-    1, dispersive shift......
+    1, there are some problem in solve the sin(phi/2) matrix element.
+    the reason I guess is about the eigen state be solved.
+    (but the result of other operator is true)
+    
+    2, dispersive shift......
     """
     from mpmath import coth
     
     start = 0
-    stop = 0.5
-    points = 101
+    stop = 1
+    points = 201
     levels = 9
     
     loop_size = 3
     res = np.zeros((loop_size,points))
 
-    Ej = 5
+    Ej = 3
     Ec = 1
     El = 1
     
-    fluxonium = Fluxonium(Ej,Ec,El,101)
+    fluxonium = Fluxonium(Ej,Ec,El,101,start=start,stop=stop,points=points)
     
-    #a = fluxonium.plot1Dspectrum('matrix_ele', {'operator':'phi','bra_ket':[[1,2],[0,2],[0,1]]})
-    #c = fluxonium.plot1Dspectrum('matrix_ele', {'operator':'n','bra_ket':[[1,2],[0,2],[0,1]]})
-    #b = fluxonium.plot1Dspectrum('eigen_spectrum', {'levels':levels,'transition':[False,False]},-1,1,101)
-    #d = fluxonium.plot1Dspectrum('T1_die_loss', {'El_array':[0.5,0.75]},0,0.5,201)
-        
+    #a = fluxonium.plot1Dspectrum('matrix_ele', {'operator':'phi','bra_ket':[[0,1],[0,2]]})
+    #c = fluxonium.plot1Dspectrum('matrix_ele', {'operator':'n','bra_ket':[[1,2],[0,2]]})
+    #b = fluxonium.plot1Dspectrum('eigen_spectrum', {'levels':levels,'transition':[True,True]})
+    #d = fluxonium.plot1Dspectrum('T1_die_loss', {'El_array':[0.5,0.75]})
+    #e = fluxonium.plot1Dspectrum('T_dephasing', {'transition':[1,2]})
+    f = fluxonium.plot1Dspectrum('matrix_ele', {'operator':'sin(phi/2)','bra_ket':[[0,1]]})
+    #g = fluxonium.plot1Dspectrum('T_qp_sing_junc', {'xqp_list':[4e-7],'transition':[0,1]})
+    #h = fluxonium.plot1Dspectrum('T_qp_array', {'xqp_list':[4e-7],'transition':[0,1]})
+    '''
+    temp1 = fluxonium.T_qp_array([4e-7],[0,1])[0]
+    temp2 = fluxonium.T_qp_sing_junc([4e-7],[0,1])[0]
+    res = []
+    for i in range(201):
+        if temp1[i] >= temp2[i]:
+            res.append(temp2[i])
+        else:
+            res.append(temp1[i])
+    '''
